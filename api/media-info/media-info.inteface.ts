c32a2b94e7065706c335a59c778e5f1de49849eb
azhivaeva <azhivaeva@bestmood.tech>
2020-12-17 01:05:09 +0300

    - Added Initialization Lambda and MediaInfo interface

--- api/media-info/media-info.inteface.ts
+++ api/media-info/media-info.inteface.ts
@@ -5,3 +5,25 @@
 export interface MediaInfoUrl {
   url: string;
 }
+
+export interface MediaInfo {
+  VideoCount: string;
+  AudioCount: string;
+  FileExtension: string;
+  Format: string;
+  Format_Profile: string;
+  CodecID: string;
+  CodecID_Compatible: string;
+  FileSize: string;
+  Duration: string;
+  OverallBitRate_Mode: string;
+  OverallBitRate: string;
+  FrameRate: string;
+  FrameCount: string;
+  StreamSize: string;
+  HeaderSize: string;
+  DataSize: string;
+  FooterSize: string;
+  IsStreamable: string;
+  Encoded_Application: string;
+}
